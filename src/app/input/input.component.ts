c016abadffd9e54c8853403a33058c7475b776c6
Jerika Kanz <jerikacapila@gmail.com>
2022-12-12 16:38:45 +0100

    Create decryption method

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -20,12 +20,33 @@ export class InputComponent {
       .split('')
       .map((letter) => {
         let letterIndex = this.alphabet.indexOf(letter) + this.shiftValue;
-        letterIndex >= this.alphabet.length
+        letter === ' '
+          ? encryptedText.push(' ')
+          : letterIndex >= this.alphabet.length
           ? encryptedText.push(
               this.alphabet[letterIndex - this.alphabet.length]
             )
           : encryptedText.push(this.alphabet[letterIndex]);
-        console.log((this.encryptedMessage = encryptedText.join('')));
+        this.encryptedMessage = encryptedText.join('');
+      });
+  }
+
+  decryptMessage() {
+    let decryptedText: string[] = [];
+    let reversedAlphabet = [...this.alphabet].reverse();
+    this.message
+      .toLowerCase()
+      .split('')
+      .map((letter) => {
+        let letterIndex = reversedAlphabet.indexOf(letter) + this.shiftValue;
+        letter === ' '
+          ? decryptedText.push(' ')
+          : letterIndex >= reversedAlphabet.length
+          ? decryptedText.push(
+              reversedAlphabet[letterIndex - reversedAlphabet.length]
+            )
+          : decryptedText.push(reversedAlphabet[letterIndex]);
+        this.encryptedMessage = decryptedText.join('');
       });
   }
 }
