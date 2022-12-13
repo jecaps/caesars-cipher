c1f7d2ac3caa80ac9672e3e369c33725127d4ffe
Jerika Kanz <jerikacapila@gmail.com>
2022-12-13 11:38:51 +0100

    Import Material Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule } from '@angular/material/input';
-import { MatRadioModule } from '@angular/material/radio';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { InputComponent } from './input/input.component';
 import { FormsModule } from '@angular/forms';
-import { RadioButtonsComponent } from './radio-buttons/radio-buttons.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [AppComponent, InputComponent, RadioButtonsComponent],
+  declarations: [AppComponent, InputComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -19,6 +20,8 @@ import { RadioButtonsComponent } from './radio-buttons/radio-buttons.component';
     FormsModule,
     MatInputModule,
     MatRadioModule,
+    MatButtonToggleModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
