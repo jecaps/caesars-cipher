0d5f3d81446fae1a0ac91ac10cce6e5628b2e305
Jerika Kanz <jerikacapila@gmail.com>
2022-12-13 11:34:38 +0100

    Incorporate encryption and decryption method into one

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -9,44 +9,28 @@ export class InputComponent {
   alphabet: string[] = [...'abcdefghijklmnopqrstuvwxyz'];
   shiftValue: number = 0;
   message: string = '';
+  transformationType: string = 'Encrypt';
+  transformedMessage: string = '';
 
-  decryptedMessage: string = '';
-  encryptedMessage: string = '';
-
-  encryptMessage() {
-    let encryptedText: string[] = [];
+  transformMessage(transformationType: string) {
+    let transformedText: string[] = [];
+    let copiedAlphabet =
+      transformationType === 'Decrypt'
+        ? [...this.alphabet].reverse()
+        : [...this.alphabet];
     this.message
       .toLowerCase()
       .split('')
       .map((letter) => {
-        let letterIndex = this.alphabet.indexOf(letter) + this.shiftValue;
+        let letterIndex = copiedAlphabet.indexOf(letter) + this.shiftValue;
         letter === ' '
-          ? encryptedText.push(' ')
-          : letterIndex >= this.alphabet.length
-          ? encryptedText.push(
-              this.alphabet[letterIndex - this.alphabet.length]
+          ? transformedText.push(' ')
+          : letterIndex >= copiedAlphabet.length
+          ? transformedText.push(
+              copiedAlphabet[letterIndex - copiedAlphabet.length]
             )
-          : encryptedText.push(this.alphabet[letterIndex]);
-        this.encryptedMessage = encryptedText.join('');
-      });
-  }
-
-  decryptMessage() {
-    let decryptedText: string[] = [];
-    let reversedAlphabet = [...this.alphabet].reverse();
-    this.message
-      .toLowerCase()
-      .split('')
-      .map((letter) => {
-        let letterIndex = reversedAlphabet.indexOf(letter) + this.shiftValue;
-        letter === ' '
-          ? decryptedText.push(' ')
-          : letterIndex >= reversedAlphabet.length
-          ? decryptedText.push(
-              reversedAlphabet[letterIndex - reversedAlphabet.length]
-            )
-          : decryptedText.push(reversedAlphabet[letterIndex]);
-        this.encryptedMessage = decryptedText.join('');
+          : transformedText.push(copiedAlphabet[letterIndex]);
+        this.transformedMessage = transformedText.join('');
       });
   }
 }
