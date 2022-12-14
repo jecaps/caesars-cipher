48fd726b227aff205f32363bfe189c09e921eca3
Jerika Kanz <jerikacapila@gmail.com>
2022-12-14 14:55:40 +0100

    Optimize Code

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -12,10 +12,10 @@ export class InputComponent {
   transformationType: string = 'Encrypt';
   transformedMessage: string = '';
 
-  transformMessage(transformationType: string) {
+  transformMessage() {
     let transformedText: string[] = [];
     let copiedAlphabet =
-      transformationType === 'Decrypt'
+      this.transformationType === 'Decrypt'
         ? [...this.alphabet].reverse()
         : [...this.alphabet];
     this.message
@@ -23,13 +23,23 @@ export class InputComponent {
       .split('')
       .map((letter) => {
         let letterIndex = copiedAlphabet.indexOf(letter) + this.shiftValue;
-        letter === ' '
-          ? transformedText.push(' ')
-          : letterIndex >= copiedAlphabet.length
-          ? transformedText.push(
-              copiedAlphabet[letterIndex - copiedAlphabet.length]
-            )
-          : transformedText.push(copiedAlphabet[letterIndex]);
+        if (letter === ' ') {
+          transformedText.push(' ');
+        } else if (letterIndex >= copiedAlphabet.length) {
+          transformedText.push(
+            copiedAlphabet[letterIndex - copiedAlphabet.length]
+          );
+        } else {
+          transformedText.push(copiedAlphabet[letterIndex]);
+        }
+
+        // letter === ' '
+        //   ? transformedText.push(' ')
+        //   : letterIndex >= copiedAlphabet.length
+        // ? transformedText.push(
+        //     copiedAlphabet[letterIndex - copiedAlphabet.length]
+        //   )
+        //   : transformedText.push(copiedAlphabet[letterIndex]);
         this.transformedMessage = transformedText.join('');
       });
   }
