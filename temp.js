daad3f49138719edc6877208a65ed61c8af5537f
Jerika Kanz <jerikacapila@gmail.com>
2023-01-10 11:24:56 +0100

    First Encryption test

--- temp.js
+++ temp.js
@@ -1,25 +1,24 @@
 const { By, Key, Builder } = require("selenium-webdriver");
+const assert = require("chai").assert;
 
-async function example() {
+const example = async () => {
   let driver = await new Builder().forBrowser("chrome").build();
 
-  try {
-    await driver.get("http://localhost:4200/");
+  await driver.get("http://localhost:4200/");
 
-    await driver
-      .findElement(By.className("shift-value"))
-      .sendKeys(3, Key.RETURN);
-    await driver
-      .findElement(By.className("message"))
-      .sendKeys("Hallo Zusammen", Key.RETURN);
-    await driver.findElement(By.className("transform-btn")).click();
+  await driver.findElement(By.className("shift-value")).sendKeys(3, Key.RETURN);
+  await driver
+    .findElement(By.className("message"))
+    .sendKeys("Hallo Zusammen", Key.RETURN);
+  await driver.findElement(By.className("transform-btn")).click();
 
-    if (await driver.findElement(By.className("output")).isDisplayed()) {
-      console.log("Test Passed");
-    }
-  } finally {
-    driver.quit();
+  if (await driver.findElement(By.className("output")).isDisplayed()) {
+    let expextedText = "kdoor cxvdpphq";
+    let outputText = await driver.findElement(By.className("output")).getText();
+    assert.equal(outputText, "kdoor cxvdpphq");
+    console.log("TEST PASSED");
   }
-}
+  await driver.quit();
+};
 
 example();
